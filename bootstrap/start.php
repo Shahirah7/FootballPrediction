6a8b4cc985de403277af364839cf65632ac85a7a
Ali Fazel <alifazel@Alis-MacBook-Pro-Retina.local>
2014-08-14 22:34:05 +0100

    Add brief + hostname to local dev environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'local' => array('homestead', 'Alis-MacBook-Pro-Retina.local'),
 
 ));
 
