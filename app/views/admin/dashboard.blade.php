301d2dd8ba05b7275083413262b96b2772a64541
Ali Fazel <alifazel.work@gmail.com>
2014-08-17 14:08:37 +0100

    Add Admin View

---
+++ app/views/admin/dashboard.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="starter-template">
+        <h1>Admin Dashboard</h1>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in leo leo. Integer ac semper justo.
+        Aenean dapibus in lorem a ullamcorper. Quisque accumsan erat diam, eu posuere turpis fringilla nec. Etiam dapibus blandit 
+        sagittis. Quisque sollicitudin, augue at tempor lobortis, ipsum neque pulvinar orci, id auctor sapien nisi quis ligula. 
+        Nam id ultrices erat.</p>
+
+        <ul>
+	        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in leo leo. Integer ac semper justo. 
+			Aenean dapibus in lorem a ullamcorper. Quisque accumsan erat diam, eu posuere turpis fringilla nec. 
+			Etiam dapibus blandit sagittis. Quisque sollicitudin, augue at tempor lobortis, ipsum neque pulvinar orci, 
+			id auctor sapien nisi quis ligula. Nam id ultrices erat.</li>
+
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in leo leo. Integer ac semper justo. 
+			Aenean dapibus in lorem a ullamcorper. Quisque accumsan erat diam, eu posuere turpis fringilla nec. 
+			Etiam dapibus blandit sagittis. Quisque sollicitudin, augue at tempor lobortis, ipsum neque pulvinar orci, 
+			id auctor sapien nisi quis ligula. Nam id ultrices erat.</li>
+
+			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in leo leo. Integer ac semper justo. 
+			Aenean dapibus in lorem a ullamcorper. Quisque accumsan erat diam, eu posuere turpis fringilla nec. 
+			Etiam dapibus blandit sagittis. Quisque sollicitudin, augue at tempor lobortis, ipsum neque pulvinar orci, 
+			id auctor sapien nisi quis ligula. Nam id ultrices erat.</li>
+		</ul>
+    </div>
+@stop
+   
